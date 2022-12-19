002ed09793dc785d1584a5045e5d63515e095f74
Anton <anton.stepanov97@mail.ru>
2022-12-19 22:19:04 +0600

    Add Task 13_1

--- Task 13/Program.cs
+++ Task 13/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int number = new Random().Next(100,1000);
+Console.WriteLine($"Сгенерированное число {number}");
+int n1 = number / 100;
+int n3 = number % 10;
+Console.WriteLine(n1 * 10 + n3 * 1);
