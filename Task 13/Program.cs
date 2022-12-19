a5db24ff0e99b93b498f0eaf8b9679feadd42a6a
Anton <anton.stepanov97@mail.ru>
2022-12-19 23:22:07 +0600

    Add Task 13_2

--- Task 13/Program.cs
+++ Task 13/Program.cs
@@ -1,6 +1,8 @@
 ﻿Console.Clear();
 int number = new Random().Next(100,1000);
 Console.WriteLine($"Сгенерированное число {number}");
-int n1 = number / 100;
-int n3 = number % 10;
-Console.WriteLine(n1 * 10 + n3 * 1);
+int n1 = number % 100;
+if (n1 > 0)
+    Console.WriteLine(n1);
+else 
+    Console.WriteLine("no");
