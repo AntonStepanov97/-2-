6794c2c7ba1a0ea4a25248708b966b791478564f
Anton <anton.stepanov97@mail.ru>
2022-12-19 21:44:21 +0600

    Add Task 10_1

--- Task 10/Program.cs
+++ Task 10/Program.cs
@@ -1,3 +1,9 @@
 ﻿Console.Clear();
-inter number = new random().Next(10,100);
-Console.WreteLine($"Сгенирированное число: {number}");
+int number = new Random().Next(10,100);
+Console.WriteLine($"Сгенирированное число: {number}");
+int n1 = number / 10;
+int n2 = number % 10;
+if (n1 > n2)
+    Console.WriteLine(n1);
+else
+    Console.WriteLine(n2);
