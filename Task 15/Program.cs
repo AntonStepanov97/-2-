e853804fa0d29a0a762f0f8bb371ce21cf768f4f
Anton <anton.stepanov97@mail.ru>
2022-12-23 21:23:18 +0600

    Add Task 15

---
+++ Task 15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите Число ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n<6) 
+    Console.WriteLine("не выходной");
+else    
+    Console.WriteLine("выходной");
